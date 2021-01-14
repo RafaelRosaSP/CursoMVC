b0e7fc2a3377b4075b94ffc83a1808277de0796f
RafaelRosaSPgit config --global user.name RafaelRosaSPgit config --global user.username RafaelRosaSP <rafael@pilotomaster.com.br>
2021-01-14 15:11:59 -0300

    Projeto finalizado a tabela de protudos e descricao

--- CursoMVC/Models/Produto.cs
+++ CursoMVC/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace CursoMVC.Models
     public class Produto
     {
         public int Id { get; set; }
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
+        [Range(1, 10, ErrorMessage = "Valor forada faixa")]
         public int Quantidade { get; set; }
+        
         public int CategoriaId { get; set; }
         public Categoria Categoria { get; set; }
 
