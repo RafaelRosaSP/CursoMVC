b11c6b876d885cb386e11b4ac561128f15354aee
RafaelRosaSPgit config --global user.name RafaelRosaSPgit config --global user.username RafaelRosaSP <rafael@pilotomaster.com.br>
2021-01-14 16:32:07 -0300

    Projeto parte 2

--- CursoMVC/Models/Categoria.cs
+++ CursoMVC/Models/Categoria.cs
@@ -13,7 +13,7 @@ namespace CursoMVC.Models
         [Required(ErrorMessage = "O campo descrição é obrigatório")]
 
         public string Descricao { get; set; }
-        public List <Produto> Produtos { get; set; }
+      
 
 
     }
